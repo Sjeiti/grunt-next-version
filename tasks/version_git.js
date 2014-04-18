7a4ed07039555b0bd9f71da1bec284be634dc23e
Ron Valstar <sjeiti@gmail.com>
2014-04-18 11:20:56 +0200

    better handling when source is not under GIT

--- tasks/version_git.js
+++ tasks/version_git.js
@@ -26,60 +26,74 @@ module.exports = function (grunt) {
 			})
 			,iFiles = aFiles.length
 		;
-		// Iterate over all specified file groups.
-		aFiles.forEach(function(src,i) {
-			var bLastFile = i===iFiles-1
-				,sSource = fs.readFileSync(src).toString()
-				// the current version
-				,sVersion = sSource.match(oOptions.regex).pop()
-				,aVersion = sVersion.split('.')
-				,oCurVersion = {
-					major: aVersion[0]
-					,minor: aVersion[1]
-					,revision: aVersion[2]
+		// check if GIT is installed for project
+		if (oOptions.git) {
+			exec('git rev-list HEAD --count', function(error,stdout){//,stderr
+				var aMatch = stdout.match(/\d+/)
+					,bGIT = !!aMatch;
+				if (bGIT) {
+					iterateFiles(aMatch.pop());
+				} else {
+					console.warn('GIT not found'); // log
+					done(false);
 				}
-				// the new version
-				,sNewMajor = newNumber('major')
-				,sNewMinor = newNumber('minor')
-				,sNewRevision = newNumber('revision')
-				,bMajor = oCurVersion.major!==sNewMajor
-				,bMinor = oCurVersion.minor!==sNewMinor
-				,bRevision = oCurVersion.revision!==sNewRevision
-			;
-			if (oOptions.git) {
-				exec('git rev-list HEAD --count', function(error,stdout){//,stderr
-					var sGitRevision = stdout.match(/\d+/).pop();
-					if (oCurVersion.revision!==sGitRevision) {
-						sNewRevision = sGitRevision;
+			});
+		} else {
+			iterateFiles();
+		}
+		// Iterate over all specified file groups.
+		function iterateFiles(gitRevision){
+			aFiles.forEach(function(src,i) {
+				var bLastFile = i===iFiles-1
+					,sSource = fs.readFileSync(src).toString()
+					// the current version
+					,sVersion = sSource.match(oOptions.regex).pop()
+					,aVersion = sVersion.split('.')
+					,oCurVersion = {
+						major: aVersion[0]
+						,minor: aVersion[1]
+						,revision: aVersion[2]
+					}
+					// the new version
+					,sNewMajor = newNumber('major')
+					,sNewMinor = newNumber('minor')
+					,sNewRevision = newNumber('revision')
+					,bMajor = oCurVersion.major!==sNewMajor
+					,bMinor = oCurVersion.minor!==sNewMinor
+					,bRevision = oCurVersion.revision!==sNewRevision
+				;
+				if (oOptions.git) {
+					if (oCurVersion.revision!==gitRevision) {
+						sNewRevision = gitRevision;
 						saveVersion();
 					} else if (bMajor||bMinor) {
 						saveVersion();
 					} else {
 						dontSaveVersion();
 					}
-				});
-			} else if (bMajor||bMinor||bRevision) {
-				saveVersion();
-			} else {
-				dontSaveVersion();
-			}
-			function newNumber(type){
-				var oParam = grunt.option(type)
-					,oSet = oOptions[type]
-					,oCur = oCurVersion[type];
-				return oParam!==undefined?oParam:(oSet===true?''+(parseInt(oCur,10)+1):(oSet===false?oCur:oSet));
-			}
-			function saveVersion(){
-				var sNewVersion = sNewMajor+'.'+sNewMinor+'.'+sNewRevision;
-				sSource = sSource.replace(oOptions.regex,sNewVersion);
-				fs.writeFileSync(src,sSource);
-				grunt.log.writeln('File \''+src+'\' updated from',sVersion,'to',sNewVersion);
-				bLastFile&&done(true);
-			}
-			function dontSaveVersion(){
-				grunt.log.writeln('File \''+src+'\' is up to date',sVersion);
-				bLastFile&&done(true);
-			}
-		});
+				} else if (bMajor||bMinor||bRevision) {
+					saveVersion();
+				} else {
+					dontSaveVersion();
+				}
+				function newNumber(type){
+					var oParam = grunt.option(type)
+						,oSet = oOptions[type]
+						,oCur = oCurVersion[type];
+					return oParam!==undefined?oParam:(oSet===true?''+(parseInt(oCur,10)+1):(oSet===false?oCur:oSet));
+				}
+				function saveVersion(){
+					var sNewVersion = sNewMajor+'.'+sNewMinor+'.'+sNewRevision;
+					sSource = sSource.replace(oOptions.regex,sNewVersion);
+					fs.writeFileSync(src,sSource);
+					grunt.log.writeln('File \''+src+'\' updated from',sVersion,'to',sNewVersion);
+					bLastFile&&done(true);
+				}
+				function dontSaveVersion(){
+					grunt.log.writeln('File \''+src+'\' is up to date',sVersion);
+					bLastFile&&done(true);
+				}
+			});
+		}
 	});
 };
